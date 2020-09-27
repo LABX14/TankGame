5c7f5e3b1b078d3e07c20f5e978b29e525f48a98
Kaulana Lee <kaulee@uat.edu>
2020-09-26 17:09:33 -0700

    Added in a timer script

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Timer : MonoBehaviour
+{
+    public float timeBetweenBooms;
+    public float countdown;
+    public float timeForNextBoom;
+    public float timeOfLastBoom;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Start our countdown at max
+        countdown = timeBetweenBooms;
+
+        // Set the time that we are allowed to boom
+        timeForNextBoom = Time.time + timeBetweenBooms;
+
+        // Set the time we last boomed
+        timeOfLastBoom = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.time >= timeForNextBoom + timeBetweenBooms)
+        {
+            Debug.Log(" Boom ");
+            // Set the time we last boomed
+            timeOfLastBoom = Time.time;
+        }
+    }
+}
